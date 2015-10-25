671beee5e6985a83175a3e18cb95192553e16dcb
DSIW <dsiw@dsiw-it.de>
2015-10-26 00:36:54 +0100

    Improve urls

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     # GET /
     url(r'^$', views.StartPageView.as_view(), name='startPage'),
-    url(r'^archives/archives.html', views.archivesPageView, name='archivesPage'),
-    url(r'^archives/(?P<book_id>[0-9]+)/archivesedit.html', views.archivesEditPageView, name='archivesEditPage'),
+    url(r'^archive/', views.archivesPageView, name='archivesPage'),
+    url(r'^books/(?P<book_id>[0-9]+)/edit', views.archivesEditPageView, name='archivesEditPage'),
 ]
