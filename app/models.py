8c32bf82575dc0761e6425541150aacd3886895b
Marie <s0531431@htw-berlin.de>
2015-10-27 21:16:46 +0100

    Refactor book model

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class Book(models.Model):
     name= models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     language = models.CharField(max_length=100)
-    releaseDate = models.DateTimeField('release_date')
+    releaseDate = models.DateField('release_date')
     pageNumber = models.IntegerField(default=1)
     isbn10 = models.CharField(max_length=100)
     isbn13 = models.CharField(max_length=100)
