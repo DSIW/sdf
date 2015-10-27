d8809800b2d70281bd8a655d60d580db2868ddc9
DSIW <dsiw@dsiw-it.de>
2015-10-27 10:51:43 +0100

    Show errors on edit book form

--- app/views.py
+++ app/views.py
@@ -58,21 +58,20 @@ def archivesEditPageView(request, book_id):
     '''
     template_name = 'app/archives_edit.html'
     book = Book.objects.get(pk=book_id);
-    bookForm = BookForm(instance=book)
+    form = BookForm(instance=book)
 
-    response = render_to_response(template_name, {
-        "form": bookForm,
-        "book": book,
-    },  RequestContext(request))
     if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
         try:
-            form = BookForm(request.POST, instance=Book.objects.get(pk=book_id))
             form.save()
             messages.add_message(request, messages.SUCCESS, 'Das Buch wurde erfolgreich aktualisiert!')
-            response = HttpResponseRedirect(reverse('archivesPage'))
+            return HttpResponseRedirect(reverse('archivesPage'))
         except ValueError as e:
             messages.add_message(request, messages.ERROR, 'Das Buch konnte leider nicht aktualisiert werden!')
-    return response
+    return render_to_response(template_name, {
+        "form": form,
+        "book": book,
+    },  RequestContext(request))
 
 
 def deleteBook(request, id):
