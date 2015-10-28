d69bb65e6f6a9aa86f4f7330d8624aa806dbac12
DSIW <dsiw@dsiw-it.de>
2015-10-28 20:47:52 +0100

    Add registration form for user

--- app/urls.py
+++ app/urls.py
@@ -17,4 +17,7 @@ urlpatterns = [
     url(r'^archive/', views.archivesPageView, name='archivesPage'),
     url(r'^books/(?P<book_id>[0-9]+)/edit', views.archivesEditPageView, name='archivesEditPage'),
     url(r'^books/(?P<id>[0-9]+)/delete$', views.deleteBook, name='deleteBook'),
+
+    # accounts
+    url(r'^accounts/register/$', views.register_user, name='register'),
 ]
