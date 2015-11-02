b0f9bf8c8a062ed37d242968344a9e0d9ac7a6d9
DSIW <dsiw@dsiw-it.de>
2015-11-02 19:16:41 +0100

    Restyle alert boxes

--- static/js/init.js
+++ static/js/init.js
@@ -7,4 +7,8 @@ jQuery(function(){
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
+
+  $('.js-alert-hide').on('click', function(event) {
+    $(this).parents('.alert').fadeOut();
+  });
 });
