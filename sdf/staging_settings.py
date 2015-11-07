7b540f9f0fd48e6c5535aa34b1333ad6ebc12d46
Jens Ottmann <ottmann.jens@googlemail.com>
2015-11-08 00:17:03 +0100

    add staging settings

--- sdf/staging_settings.py
+++ sdf/staging_settings.py
@@ -1,6 +1,8 @@
 from sdf.base_settings import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.htw-berlin.de', # Allow domain and subdomains
+]
