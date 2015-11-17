d6e0e60fcc71969fb24f7239d63a464717da6edb
Mario Link <s0536176@htw-berlin.de>
2015-11-17 13:48:18 +0100

    *

--- app/urls.py
+++ app/urls.py
@@ -11,5 +11,11 @@ urlpatterns = [
 
     # session
     url(r'^login$', auth_views.login, name='login'),
-    url(r'^logout$', auth_views.logout, name='url'),
+    url(r'^logout$', auth_views.logout, name='logout'),
+    url(r'^imprint$', views.page_info, {"template_name":"app/page_info.html","title":"Impressum"}, name='imprint'),
+    url(r'^agb$', views.page_info, {"template_name":"app/page_info.html","title":"AGB"}, name='agb'),
+    url(r'^privacy$', views.page_info, {"template_name":"app/page_info.html","title":"Datenschutz"}, name='privacy'),
+    url(r'^affiliate$', views.page_info, {"template_name":"app/page_info.html","title":"Affiliate"}, name='affiliate'),
+    url(r'^team$', views.page_info, {"template_name":"app/page_info.html","title":"Team"}, name='team'),
+    url(r'^job$', views.page_info, {"template_name":"app/page_info.html","title":"Job"}, name='job'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.base import TemplateView
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 class StartPageView(TemplateView):
     template_name = 'app/start.html'
@@ -7,3 +9,5 @@ class StartPageView(TemplateView):
         context = super(StartPageView, self).get_context_data(**kwargs)
         return context
 
+def page_info(request, template_name, title):
+    return render_to_response(template_name, {"title":title}, RequestContext(request))
