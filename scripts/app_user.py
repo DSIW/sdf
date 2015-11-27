f3f5a650f06dde9a5680b2c7203044013579fd76
Max <dsiw@dsiw-it.de>
2015-11-27 13:26:01 +0100

    Add buyer to seed data

--- scripts/app_user.py
+++ scripts/app_user.py
@@ -152,14 +152,30 @@ def import_data():
     auth_user_1.password = 'pbkdf2_sha256$20000$f49atNcSl4xt$jBFjdFzW7X6ownYonQ7LK6EzMwshtYtLUxtdMqWGwks='
     auth_user_1.last_login = dateutil.parser.parse("2015-11-08T14:22:59.793180+00:00")
     auth_user_1.is_superuser = False
-    auth_user_1.username = 'mustermann'
+    auth_user_1.username = 'max'
     auth_user_1.first_name = 'Max'
     auth_user_1.last_name = 'Mustermann'
     auth_user_1.email = 'max@mustermann.de'
     auth_user_1.emailConfirm = True
-    auth_user_1.paypal = auth_user_1.email
+    auth_user_1.paypal = 'max@mustermann.de'
     auth_user_1.location = 'Berlin'
     auth_user_1.is_staff = False
     auth_user_1.is_active = True
     auth_user_1.date_joined = dateutil.parser.parse("2015-11-08T14:22:49.437731+00:00")
     auth_user_1 = importer.save_or_locate(auth_user_1)
+
+    auth_user_2 = User()
+    auth_user_2.password = 'pbkdf2_sha256$20000$f49atNcSl4xt$jBFjdFzW7X6ownYonQ7LK6EzMwshtYtLUxtdMqWGwks='
+    auth_user_2.last_login = dateutil.parser.parse("2015-11-08T14:22:59.793180+00:00")
+    auth_user_2.is_superuser = False
+    auth_user_2.username = 'martin'
+    auth_user_2.first_name = 'Martin'
+    auth_user_2.last_name = 'Mustermann'
+    auth_user_2.email = 'martin@mustermann.de'
+    auth_user_2.emailConfirm = True
+    auth_user_2.paypal = 'martin@mustermann.de'
+    auth_user_2.location = 'Hamburg'
+    auth_user_2.is_staff = False
+    auth_user_2.is_active = True
+    auth_user_2.date_joined = dateutil.parser.parse("2015-11-08T14:22:49.437731+00:00")
+    auth_user_2 = importer.save_or_locate(auth_user_2)
