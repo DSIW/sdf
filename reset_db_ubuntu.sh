f6afb3c04440a72216244b76ccc59f218665e34a
Max <dsiw@dsiw-it.de>
2015-11-30 22:56:31 +0100

    Remove migration files in reset_db.sh

--- reset_db_ubuntu.sh
+++ reset_db_ubuntu.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Remove migration files
+for d in $(find . -type d -name "migrations"); do find $d -iname "*.py" -and -not -iname "__init__.py" -exec rm -f {} \;; done
+
 rm db.sqlite3
 python3 manage.py makemigrations
 python3 manage.py migrate
