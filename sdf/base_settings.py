e2a7ca9bb29e9370e95653cd4d42aaf69d061e21
Max <dsiw@dsiw-it.de>
2015-12-14 18:22:34 +0100

    Show unread notifications in navbar

--- sdf/base_settings.py
+++ sdf/base_settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.core.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
                 'django.core.context_processors.media',
+                'app_notification.context_processors.unread_notification_exist',
             ],
         },
     },
