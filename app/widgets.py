93763a4519dff8ee4c5829d895bc9fb5b89d33bf
Jens Ottmann <ottmann.jens@googlemail.com>
2015-12-24 00:10:52 +0100

    cache image, when validationerror is raised. close #90

--- app/widgets.py
+++ app/widgets.py
@@ -1,29 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from django.forms.widgets import FileInput
+from file_resubmit.admin import AdminResubmitImageWidget
+
 
 from sdf import settings
 
 
-class CustomFileInput(FileInput):
+class CustomFileInput(AdminResubmitImageWidget):
     input_type = 'file'
 
     def render(self, name=None, value=None, attrs=None, single_attrs=None):
-        self = ''
-        if value:
-            self +='<img src="/media/' + str(value) + '" />'
-        else:
-            value = ''
-        self += '''
+        output = ''
+        output += '''
             <div class="file-field input-field">
               <div class="btn">
                 <span>File</span>
                 <input type="file" id="''' + name + '''" name="''' + name + '''" >
               </div>
-              <div class="file-path-wrapper">
-                <input class="file-path validate" value="''' + str(value) + '''" type="text">
-              </div>
+              <div class="file-path-wrapper">'''
+        output += self.output_extra_data(value)
+        output += '''</div>
             </div>
           <label class="active" for="''' + name + '''"><br />Bild (max. ''' + str(settings.FILESIZE_LIMIT_MB) + ''' MB)</label>
-'''
-        return self
+        '''
+
+        return output
