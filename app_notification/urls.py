449b9966880a347663aaccfcad465640ce650ba5
Max <dsiw@dsiw-it.de>
2016-01-05 17:18:04 +0100

    Send email if new notifications are created

--- app_notification/urls.py
+++ app_notification/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^notifications/(?P<id>[0-9]+)/read/', views.read_notification, name='read-notification'),
+    url(r'^notifications/toggleEmail/', views.notificationEmailToggle, name='notificationEmailToggle'),
     url(r'^notifications/', views.notificationPageView, name='notificationsPage'),
     url(r'^notificationSendBook/(?P<id>[0-9]+)', views.notificationSendBookPageView, name='notificationsSendBookPage'),
 ]
