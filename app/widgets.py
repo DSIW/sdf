a75db996025d0e9ac7140a9142b738909982dab0
Jens Ottmann <ottmann.jens@googlemail.com>
2016-01-09 20:49:40 +0100

    better image upload widget

--- app/widgets.py
+++ app/widgets.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from file_resubmit.admin import AdminResubmitImageWidget
-
+import re
 
 from sdf import settings
 
@@ -11,6 +11,11 @@ class CustomFileInput(AdminResubmitImageWidget):
 
     def render(self, name=None, value=None, attrs=None, single_attrs=None):
         output = ''
+
+        if value is not None and re.match("^images\/(books\/book_|user\/user_)\d+\.", str(value)) is not None:
+            output +='<img src="/media/' + str(value) + '" />'
+        else:
+            output = ''
         output += '''
             <div class="file-field input-field">
               <div class="btn">
@@ -18,7 +23,10 @@ class CustomFileInput(AdminResubmitImageWidget):
                 <input type="file" id="''' + name + '''" name="''' + name + '''" >
               </div>
               <div class="file-path-wrapper">'''
-        output += self.output_extra_data(value)
+        if self.output_extra_data(value):
+            output += self.output_extra_data(value)
+        else:
+            output += '''<input class="file-path validate" value="''' + str(value) + '''" type="text">'''
         output += '''</div>
             </div>
           <label class="active" for="''' + name + '''"><br />Bild (max. ''' + str(settings.FILESIZE_LIMIT_MB) + ''' MB)</label>
