c3bfcf3f3fb007467c787904845da0998bce404e
Max <dsiw@dsiw-it.de>
2016-01-09 22:54:08 +0100

    Add production settings

---
+++ sdf/production_settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from sdf.base_settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    '.htw-berlin.de', # Allow domain and subdomains
+]
+
+# Paypal
+ENDPOINT = "https://ws15sdf-b.f4.htw-berlin.de"
