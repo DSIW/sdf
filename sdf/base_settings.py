47ca6919746f358963343327f3ec681b56dbb8ce
Daniel Chumak <chumak.daniel.dev@gmail.com>
2016-01-16 14:13:17 +0100

    css cleanup, various ui improvements

--- sdf/base_settings.py
+++ sdf/base_settings.py
@@ -179,7 +179,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-MAX_ELEMENTS_PER_PAGE = 12
+MAX_ELEMENTS_PER_PAGE = 8
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
